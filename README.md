4d5f20e22ddd3eb646a1437f9ae418d211b04754
Dustin Carlino <dabreegster@gmail.com>
2022-01-27 15:50:46 +0000

    Start downloading something for one sensor

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # urban_observatory_abst
 
 `poetry run python main.py`
+
+## UX feedback
+
+- Showing plots and the interpolation options super useful!
+- I figured out the URL by using Firefox's network inspector. Maybe tick a box
+  to show URLs for the current options selected?
+- Where do I find the coordinates / metadata given a sensor?
+- How do I get all sensors in an area?
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-import urllib
+import json
+import requests
+import statistics
 
 
 def main():
-    pass
+    ref = 'ca-traffic__37000007__vehicle-count_E'
+    start_time = '2021-04-22T00:00:00.000Z'
+    end_time = '2022-01-27T15:38:51.077Z'
+    resolution = '1d'
+    scrape(
+        f'https://muo-backend.cs.man.ac.uk/rawdata?ref={ref}&start_time={start_time}&end_time={end_time}&resolution={resolution}&interpolate=false&quantity=vehicle-count&cumulative=true')
 
+
+def scrape(url):
+    data = json.loads(requests.get(url).content)
+    # We don't care about the dates yet (until we know when the LTN was installed...)
+    # TODO But also ignore outliers like Christmas
+    counts = [entry['value'] for entry in data]
+    print(statistics.mean(counts))
 
 
 if __name__ == "__main__":
