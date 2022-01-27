a3287494df92832c778842f936e5bf94cc074ffb
Dustin Carlino <dabreegster@gmail.com>
2022-01-27 16:47:26 +0000

    Look for all sensors

--- main.py
+++ main.py
@@ -4,16 +4,32 @@ import statistics
 
 
 def main():
-    ref = 'ca-traffic__37000007__vehicle-count_E'
+    for url in url_to_json('https://muo-backend.cs.man.ac.uk/deployments/levenshulme-bee-network')['deployedOnPlatform']:
+        platform = url_to_json(url)
+        if platform.get('description') == 'Traffic camera':
+            for host in platform['hosts']:
+                sensor = url_to_json(host)
+                location = sensor['centroid']['geometry']['coordinates']
+                for timeseries in sensor['timeseries']:
+                    # We don't want speed
+                    if 'count' in timeseries:
+                        ref = timeseries.split('/')[-1]
+                        print(f'Sensor at {location}')
+                        scrape(ref)
+
+
+def url_to_json(url):
+    return json.loads(requests.get(url).content)
+
+
+def scrape(ref):
+    print(f'Scraping {ref}')
     start_time = '2021-04-22T00:00:00.000Z'
     end_time = '2022-01-27T15:38:51.077Z'
     resolution = '1d'
-    scrape(
-        f'https://muo-backend.cs.man.ac.uk/rawdata?ref={ref}&start_time={start_time}&end_time={end_time}&resolution={resolution}&interpolate=false&quantity=vehicle-count&cumulative=true')
+    url = f'https://muo-backend.cs.man.ac.uk/rawdata?ref={ref}&start_time={start_time}&end_time={end_time}&resolution={resolution}&interpolate=false&quantity=vehicle-count&cumulative=true'
 
-
-def scrape(url):
-    data = json.loads(requests.get(url).content)
+    data = url_to_json(url)
     # We don't care about the dates yet (until we know when the LTN was installed...)
     # TODO But also ignore outliers like Christmas
     counts = [entry['value'] for entry in data]
